d6efb8006664de37f28275e6753243d3ff6e0fc8
mayank2130 <mayankthakur1712@gmail.com>
2024-09-15 14:16:28 +0530

    Realations Screens

---
+++ app/relations/bankers/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BankerRelations = () => {
+  return (
+    <View>
+      <Text>BankerRelations</Text>
+    </View>
+  )
+}
+
+export default BankerRelations
+
+const styles = StyleSheet.create({})
