947dc92c738bc83f2b8ca13f0be142d565e55ad2
mayank2130 <mayankthakur1712@gmail.com>
2024-09-24 21:36:08 +0530

    different liscenes

--- constants/Liscence.ts
+++ constants/Liscence.ts
@@ -1,6 +1,7 @@
 export interface Liscence {
   id: number;
   name: string;
+  value: string;
   cost: number;
   description: string;
   duration: number;
@@ -8,9 +9,10 @@ export interface Liscence {
   requiredInfluence: number;
 }
 
-export const Liscence: Liscence[] = [
+export const BusinessLiscences: Liscence[] = [
   {
     id: 1,
+    value:"power",
     name: "Power Plant Business Liscence",
     cost: 10000000,
     description:
@@ -18,5 +20,25 @@ export const Liscence: Liscence[] = [
     duration: 3,
     typeofInfluence: "Politician",
     requiredInfluence: 25,
+  },
+  {
+    id: 2,
+    value:"bank",
+    name: "Banking Liscence",
+    cost: 25000000,
+    description: "For you to own a bank you must obtain this liscence.",
+    duration: 3,
+    typeofInfluence: "Politician",
+    requiredInfluence: 45,
+  },
+  {
+    id: 3,
+    value:"oil",
+    name: "Oil & Gas Liscence",
+    cost: 25000000,
+    description: "For you to own a bank you must obtain this liscence.",
+    duration: 3,
+    typeofInfluence: "Politician",
+    requiredInfluence: 65,
   },
 ];
